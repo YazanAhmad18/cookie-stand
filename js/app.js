f9a6d1a2ceece6d6dda413e08e646fdc03325d7b
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-04-12 17:19:35 +0300

    some edit

--- js/app.js
+++ js/app.js
@@ -3,17 +3,6 @@
 
 
 
-<<<<<<< HEAD
-const seattle = {
-  name:'seattle',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookie: 6.3,
-  customernum: [],
-  avgcookie:[],
-  total:0,
-  hours:['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-=======
 let hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 let sale=document.getElementById('sales');
 let table1=document.createElement('table');
@@ -21,7 +10,6 @@ sale.appendChild(table1);
 table1.setAttribute('border','1');
 table1.setAttribute('cellpadding','7');
 table1.setAttribute('align','center');
->>>>>>> class07-constructor
 
 let arrayobejcts=[];
 
@@ -161,6 +149,7 @@ function tablefooter(){
 
 
 tablefooter();
+
 
 
 
